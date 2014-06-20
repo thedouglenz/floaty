f3d6690e204361df83490dead2541b948a0c6fac
Douglas Lenz <thedouglenz@gmail.com>
2014-06-20 16:17:49 -0400

    First push

---
+++ floaty.js
@@ -0,0 +1,26 @@
+/*
+	Douglas Lenz, June 2014
+	Floaty is a div type extension custom element
+*/
+
+var floatyPrototype = Object.create(HTMLDivElement.prototype);
+
+floatyPrototype.createdCallback = function() {
+
+};
+
+floatyPrototype.attachedCallback = function() {
+
+};
+
+floatyPrototype.detachedCallback = function() {
+
+};
+
+var Floaty = document.registerElement('flo-t',
+{
+	prototype: floatyPrototype,
+	extends: 'div'
+});
+
+
